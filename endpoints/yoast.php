a623bc0f8cdb8133663fa79fafc2ea6db7a74177
Luffynas <blankcd3@gmail.com>
2024-12-05 18:51:42 +0700

    fix(index-Yoast): remove canonical url because cannot detect in post-sitemap.xml

--- endpoints/yoast.php
+++ endpoints/yoast.php
@@ -7,7 +7,6 @@ function yoast_save_seo_data( WP_REST_Request $request ) {
     $meta_description = $request->get_param( 'meta_description' );
     $focus_keyword = $request->get_param( 'focus_keyword' );
     $robots = $request->get_param( 'robots' );
-    $canonical_url = $request->get_param( 'canonical_url' );
     $schema_type = $request->get_param( 'schema_type' );
     $faq_schema = $request->get_param( 'faq_schema' );
     $facebook_title = $request->get_param( 'facebook_title' );
@@ -23,7 +22,6 @@ function yoast_save_seo_data( WP_REST_Request $request ) {
     update_post_meta( $post_id, '_yoast_wpseo_metadesc', $meta_description );
     update_post_meta( $post_id, '_yoast_wpseo_focuskw', $focus_keyword );
     update_post_meta( $post_id, '_yoast_wpseo_robots', $robots );
-    update_post_meta( $post_id, '_yoast_wpseo_canonical', $canonical_url );
     update_post_meta( $post_id, '_yoast_wpseo_schema_type', $schema_type );
     update_post_meta( $post_id, '_yoast_wpseo_faq_schema', $faq_schema );
     update_post_meta( $post_id, '_yoast_wpseo_facebook_title', $facebook_title );
