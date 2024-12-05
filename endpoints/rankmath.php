7274ac7d9385c2400691ed9221afd1df51fa7965
Luffynas <blankcd3@gmail.com>
2024-12-05 18:51:03 +0700

    fix(index): remove canonical url because cannot detect in post-sitemap.xml

--- endpoints/rankmath.php
+++ endpoints/rankmath.php
@@ -7,7 +7,6 @@ function rankmath_save_seo_data( WP_REST_Request $request ) {
     $meta_description = $request->get_param( 'meta_description' );
     $focus_keyword = $request->get_param( 'focus_keyword' );
     $robots = $request->get_param( 'robots' );
-    // $canonical_url = $request->get_param( 'canonical_url' );
     $schema_type = $request->get_param( 'schema_type' );
     $faq_schema = $request->get_param( 'faq_schema' );
     $facebook_title = $request->get_param( 'facebook_title' );
@@ -23,7 +22,6 @@ function rankmath_save_seo_data( WP_REST_Request $request ) {
     update_post_meta( $post_id, 'rank_math_description', $meta_description );
     update_post_meta( $post_id, 'rank_math_focus_keyword', $focus_keyword );
     update_post_meta( $post_id, 'rank_math_robots', $robots );
-    // update_post_meta( $post_id, 'rank_math_canonical_url', $canonical_url );
     update_post_meta( $post_id, 'rank_math_schema_type', $schema_type );
     update_post_meta( $post_id, 'rank_math_faq_schema', $faq_schema );
     update_post_meta( $post_id, 'rank_math_facebook_title', $facebook_title );
